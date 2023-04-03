30c6a25e1a21462f3afeb48cddabc5da18fa3dbc
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-04-03 12:25:45 -0400

    Minor change and adding environment files

---
+++ Flashcard-Frontend/src/environment/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    withCredentials: true,
+    baseUrl: "https://flashcardpsork.azurewebsites.net",
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '',
+    },
+};
---
+++ Flashcard-Frontend/src/environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    withCredentials: true,
+    baseUrl: "https://localhost:7041",
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:4200',
+    },
+};
